feff4f19a0dd5c42a4db12980935dd41bc82b4b7
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-03 01:52:22 -0500

    a

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,6 @@ const db = mysql.createConnection({
 app.use(express.json())
 app.use(cors())
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
   });
