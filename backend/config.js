0ab1eb0d98a692ecba664cb21bdb1a5423fbf7f8
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-03 02:06:04 -0500

    omg

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://frontend-e8bx.onrender.com/"
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://frontend-e8bx.onrender.com"
 
 export const DB_HOST = process.env.DB_HOST || "sql3.freemysqlhosting.net"
 export const DB_USER = process.env.DB_USER || "sql3711363"
