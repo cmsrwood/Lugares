80b3dddad24fa28e5edc7e5478c5b1ee0582abe0
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-03 01:44:56 -0500

    backend acess control allow methods

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,12 @@ const db = mysql.createConnection({
 
 app.use(express.json())
 app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get("/",(req,res)=>{
     res.json("Hello World from backend") 
