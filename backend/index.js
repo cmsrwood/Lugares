04971ff2799d860019bb009c0172e64c72a08bcf
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-06-03 01:50:59 -0500

    allow

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,9 @@ app.use(express.json())
 app.use(cors())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
   });
 
